a246ac5284bfb700bb4ec9c6d77d569411006550
cornalei <1591505373@qq.com>
2019-07-05 18:08:22 +0800

    封装启动appium服务

--- test_run/send_email.py
+++ test_run/send_email.py
@@ -1,13 +1,58 @@
-import unittest
-# from BSTestRunner import BSTestRunner
+import unittest,subprocess,os,socket
+from HTMLTestRunner import HTMLTestRunner
+from time import ctime
 from HTMLTestRunner import HTMLTestRunner
 import time,logging
 import smtplib                           #发送邮件模块
 from email.mime.text import MIMEText    #定义邮件内容
 from email.header import Header         #定义邮件标题
 from email.mime.multipart import MIMEMultipart  #用于传送附件
-import os
+# bat处理执行时使用
+import sys
 
+path = 'E:\\ysbb'  # 文件所在根目录
+sys.path.append(path)
+
+# 指定测试用例和测试报告的路径
+test_dir = '../test_case'
+report_dir = '../reports'
+def run_case():
+    #加载测试用例
+    discover=unittest.defaultTestLoader.discover(test_dir,pattern='test*.py') #匹配test开头的用例
+    # discover=unittest.defaultTestLoader.discover(test_dir,pattern='test_z_player.py')
+
+    #定义报告的文件格式
+    now=time.strftime('%Y-%m-%d %H_%M_%S')
+    report_name=report_dir+'/'+now+' test_report.html'
+
+    #运行用例并生成测试报告
+    with open(report_name,'wb') as f:
+        runner=HTMLTestRunner(stream=f,title='一说宝宝测试报告',description='一说宝宝安卓版自动化测试报告')
+        logging.info('start run test case...')
+        runner.run(discover)
+
+def check_port(host, port):
+    """检测指定的端口是否被占用"""
+
+    # 创建socket对象
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((host, port))
+        s.shutdown(2)
+    except OSError:
+        print('port %s is available! ' % port)
+        return False
+    else:
+        print('port %s already be in use !' % port)
+        return True
+
+def appium_start(host, port):
+    bootstrap_port = str(port + 1)
+    cmd = 'start /b appium -a ' + host + ' -p ' + str(port) + ' -bp ' + str(bootstrap_port)
+
+    print('%s at %s' % (cmd, ctime()))
+    subprocess.Popen(cmd, shell=True, stdout=open('../logs/' + str(port) + '.log', 'a'),
+                     stderr=subprocess.STDOUT)
 
 def send_mail(latest_report):
     f = open(latest_report, 'rb')
@@ -60,7 +105,6 @@ def send_mail(latest_report):
     smtp.quit()
     print("Send Email end!")
 
-
 def latest_report(report_dir):
     lists = os.listdir(report_dir)
     # 按时间顺序对该目录文件夹下面的文件进行排序
@@ -71,34 +115,27 @@ def latest_report(report_dir):
     print(file)
     return file
 
+def start_appium_action(host,port):
+    '''检测端口是否被占用，如果没有被占用则启动appium服务'''
+    if check_port(host,port):
+        run_case()
+        return True
+    else:
+        appium_start(host, port)
+        time.sleep(5)
+        return False
+
+def start_devices_action(host,port):
+    '''先检测appium服务是否启动成功，启动成功则再启动App'''
+    if start_appium_action(host,port):
+        print('finis！')
+    else:
+        run_case()
 
 if __name__ == '__main__':
-    # bat处理执行时使用
-    import sys
-
-    path = 'E:\\ysbb'  # 文件所在根目录
-    sys.path.append(path)
-
-    # 指定测试用例和测试报告的路径
-    test_dir = '../test_case'
-    report_dir = '../reports'
-
-    # 加载测试用例
-    discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')  # 匹配test开头的用例
-    # discover = unittest.defaultTestLoader.discover(test_dir, pattern='test_icon.py')
-
-    # 定义报告的文件格式
-    now = time.strftime('%Y-%m-%d %H_%M_%S')
-    report_name = report_dir + '/' + now + ' test_report.html'
-
-    # 运行用例并生成测试报告
-    with open(report_name, 'wb') as f:
-        # runner = BSTestRunner(stream=f, title='一说宝宝测试报告', description='这是一说宝宝安卓版的自动化测试报告，详情请查看附件，tks')
-        runner = HTMLTestRunner(stream=f, title='一说宝宝测试报告', description='这是一说宝宝安卓版的自动化测试报告，详情请查看附件，tks')
-        logging.info('start run test case...')
-        runner.run(discover)
-    f.close()
-
+    host = '127.0.0.1'
+    port = 4723
+    start_devices_action(host, port)
     # h获取最新测试报告
     latest_report = latest_report(report_dir)
     # 发送邮件报告
